0891bed4dac79dadce8ecb372d307510e9fdaef1
githubVivek55 <vivekpce55@outlook.com>
2018-03-26 23:55:13 +0530

    React-Redux skeleton and demo coded

--- src/components/App.js
+++ src/components/App.js
@@ -5,8 +5,10 @@ class App extends React.Component{
     render(){
         return(
             <div className="container-fluid">
+            
             <Header/>
-                {this.props.children}
+            
+            {this.props.children}
             </div>
         );
     }
